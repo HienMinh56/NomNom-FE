344c1112f8eaac352379797e2bb971d5c1b7ff6c
Fancy <dlhminh0506@gmail.com>
2024-06-28 00:58:20 +0700

    Feat: Fill data in chart

--- src/api/order.api.js
+++ src/api/order.api.js
@@ -30,4 +30,28 @@ async function getOrders(filters = {}) {
 }
 
 
-module.exports = { getOrders }
+async function dataChart(type, month, year) {
+    try {
+        const response = await axios.get(`${apiConfig.BASE_URL}/order/data-chart?type=${type}&year=${year}&month=${month}`, { httpsAgent: agent });
+
+        if (response.data.isSuccess) {
+            const data = response.data.data;
+            const chartData = {
+                labels: data.map(item => new Date(item.date).toLocaleDateString('vn-VN', { month: 'short', day: 'numeric' })),
+                datasets: [{
+                    label: "Total Amount",
+                    data: data.map(item => item.totalAmount)
+                }]
+            };
+            return { data: chartData };
+        } else {
+            return { error: 'Failed to fetch data' };
+        }
+    } catch (error) {
+        console.error('Error generating data chart:', error);
+        return { error: 'An error occurred while generating data chart' };
+    }
+}
+
+
+module.exports = { getOrders, dataChart }
