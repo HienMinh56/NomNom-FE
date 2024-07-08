6a5ead3f388b8f5823e1b6a6ad6241af42a2c20e
Fancy <minhdlh.dev@gmail.com>
2024-07-08 22:19:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -146,6 +146,6 @@ app.use((req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log(`Server is listening at http://localhost:3000`);
+app.listen(5000, () => {
+    console.log(`Server is listening at http://localhost:5000`);
 });
